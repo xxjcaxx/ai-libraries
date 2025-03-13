89107e9da67074d0aa02d983587c43182e52ab5b
xxjcaxx <xxjcaxx@yahoo.es>
2025-03-13 20:02:57 +0100

    unpack

--- c/chessintionlib/chess_aux_c.py
+++ c/chessintionlib/chess_aux_c.py
@@ -50,7 +50,8 @@ def concat_fen_legal(fen):
     #return reshaped_result
 
     compressed_tensor = torch.tensor(list(result_ptr.contents), dtype=torch.uint8, device="cuda")
-    bit_tensor = torch.unpackbits(compressed_tensor).to(dtype=torch.bool)
+    #bit_tensor = torch.unpackbits(compressed_tensor).to(dtype=torch.bool)
+    bit_tensor = ((compressed_tensor[:, None] >> torch.arange(8, device="cuda")) & 1).bool()
     bit_tensor = bit_tensor.view(77, 8, 8)
     return bit_tensor
 
