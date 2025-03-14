3625f0bb81684a8f1b0d31ca2d6d47185d6281b5
xxjcaxx <xxjcaxx@yahoo.es>
2025-03-14 12:55:01 +0100

    sdff

--- test_c.py
+++ test_c.py
@@ -1,7 +1,9 @@
 import ctypes
 import numpy as np
+import cProfile
+import pstats
 
-chess_extension = ctypes.CDLL('./c/chessintionlib/build/libchessintionlib.so') 
+chess_extension = ctypes.CDLL('./c/chessintionlib/libchessintionlib.so') 
 
 
 chess_extension.concat_fen_legal.argtypes = [ctypes.c_char_p]
@@ -18,23 +20,34 @@ def concat_fen_legal(fen):
     compressed_array = np.frombuffer(result_ptr.contents, dtype=np.uint8)
     bit_array = np.unpackbits(compressed_array).astype(np.uint8)
     array_np = bit_array.reshape(77, 8, 8)
-    np.set_printoptions(threshold=np.inf)
-    print(array_np)
+    #np.set_printoptions(threshold=np.inf)
+    #print(array_np)
 
 #concat_fen_legal('3k4/1K3B2/1BP5/1n3p2/7p/7P/7b/8 b - - 4 3')
 
 def concat_fen_legal_bits(fen):
     fen_bytes = fen.encode('utf-8')
     
-    result_ptr = chess_extension.concat_fen_legal(fen_bytes)
+    result_ptr = chess_extension.concat_fen_legal_bits(fen_bytes)
     compressed_array = np.frombuffer(result_ptr.contents, dtype=np.uint8)
     bit_array = np.unpackbits(compressed_array).astype(np.uint8)
     array_np = bit_array.reshape(77, 8, 8)
-    np.set_printoptions(threshold=np.inf)
-    print(array_np)
+    #np.set_printoptions(threshold=np.inf)
+    #print(array_np)
 
-    return array_np
+    #return array_np
 
 
 
-concat_fen_legal_bits('3k4/1K3B2/1BP5/1n3p2/7p/7P/7b/8 b - - 4 3')
+#concat_fen_legal_bits('3k4/1K3B2/1BP5/1n3p2/7p/7P/7b/8 b - - 4 3')
+
+def bench(n):
+    for i in range(n):
+        concat_fen_legal_bits('3k4/1K3B2/1BP5/1n3p2/7p/7P/7b/8 b - - 4 3')
+        concat_fen_legal('3k4/1K3B2/1BP5/1n3p2/7p/7P/7b/8 b - - 4 3')
+
+cProfile.run('bench(10000)', 'output.prof')
+
+# Print sorted profiling results (100 simulations) 19.466 seconds
+p = pstats.Stats('output.prof')
+p.strip_dirs().sort_stats('cumulative').print_stats(20)  # Top 10 slowest functions
